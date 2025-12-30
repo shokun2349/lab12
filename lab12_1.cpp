441450452d2e227760a305b25b474d08dc670d6a
shokun2349 <shokun2349@gmail.com>
2025-12-30 16:13:42 +0700

    lab12_1 real complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,15 +1,18 @@
 #include<iostream>
 #include<ctime>
 using namespace std;
-int fibonacci(int n);
+#define ll long long
+ll fibonacci(ll n);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
-int fibonacci(int n){
+ll fibonacci(ll n){
     if(n == 0) return 0;
     else if(n == 1) return 1;
     else return fibonacci(n-1) + fibonacci(n-2);
